e9d212e71d19ca8bbb5b789fdc73a0c1213e8b51
Javier Pinilla <javierpinilla@sipay.es>
2024-06-01 18:48:30 +0200

    fix

--- src/components/mobile/gallery/GalleryComponent.vue
+++ src/components/mobile/gallery/GalleryComponent.vue
@@ -135,3 +135,76 @@ export default {
   }
 }
 </style>
+
+
+<style scoped>
+.gallery-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding-bottom: 30vw;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+.gallery-container::-webkit-scrollbar {
+  display: none; /* Hide scrollbar for Chrome, Safari, and Opera */
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0 4px;
+  width: 100%;
+}
+
+.column {
+  flex: 50%;
+  max-width: 50%;
+  padding: 0 0.6vh;
+}
+
+.column img {
+  margin-top: 0.6vh;
+  vertical-align: middle;
+  width: 100%;
+  cursor: pointer;
+}
+
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  position: relative;
+}
+
+.full-image {
+  position: relative;
+  z-index: 2;
+  max-width: 65vw;
+  max-height: 90vh;
+}
+
+@media only screen and (orientation: portrait) {
+  .column {
+    flex: 100%;
+    max-width: 100%;
+  }
+  .full-image {
+    position: relative;
+    top: -2vh;
+    max-width: 85vw;
+    max-height: 85vh;
+  }
+}
+</style>
